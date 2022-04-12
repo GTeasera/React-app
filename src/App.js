afe3124b5a08eaffbe7c9be5da0340e7af53b01e
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-04-12 10:39:25 +0300

    12/04/2022

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import  React from 'react';
+import  React, {useState} from 'react';
+import Counter from "./components/Counter";
 
 function App() {
+    const [likes, setLikes] = useState(5)
+    const [value, setValue] = useState('Some Text')
+
   return (
     <div className="App">
+        <Counter/>
 
+        <h1>{value}</h1>
+        <input
+            type={"text"}
+            value={value}
+            onChange={event => setValue(event.target.value)}
+        />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-    <div>
-        <h1>Test</h1>
-        123
-    </div>,
+    <App/>,
   document.getElementById('root')
 );
