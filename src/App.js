1a2df58f9b6a276abc254fa20853cb32702bbb91
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-04-18 11:11:21 +0300

    18/04/2022

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import  React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState( [
@@ -12,9 +14,42 @@ function App() {
         {id: 2, title: 'C#', body: 'Description C#'},
         {id: 3, title: 'C++', body: 'Description C++'}
     ])
+    
+    const [title, setTitle] = useState('My Post Name')
+    /* Way with hook "useRef" */
+    const bodyInputRef = useRef();
+    /* Way with controlable component */
+    const addNewPost = (element) => {
+        element.preventDefault()
+        console.log(title)
+        console.log(bodyInputRef.current.value)
+    }
 
   return (
     <div className="App">
+        <form>
+            <MyInput
+                /* Controlable compontent */
+                value={title}
+
+                /* Двухстороннее связывание */
+                onChange={element => setTitle(element.target.value)}
+
+                type="text"
+                placeholder="Name"
+            />
+
+            /* Not controlable compontent */
+            <MyInput
+                ref={bodyInputRef}
+                type="text"
+                placeholder="Description"
+            />
+
+            <MyButton onClick={addNewPost}>Create Post</MyButton>
+            <MyButton disabled>Edit Post</MyButton>
+        </form>
+
         <PostList posts={posts} title={'123'}/>
     </div>
   );
